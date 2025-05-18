53a37cac6b44d219343cf314e028cd00de20ef12
Vianpyro <vianney@veremme.org>
2025-05-18 16:32:02 +0000

    Add Navbar component and integrate it into the App layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
-import { Link, Route, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import Login from './pages/Login'
 import Home from './pages/Home'
+import Navbar from './components/Navbar'
 import './assets/styles/App.css'
 
 function App() {
     return (
         <>
-            <Link to="/">Go to Home</Link>
-            <Link to="/login">Go to Login</Link>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/login" element={<Login />} />
-            </Routes>
+            <Navbar />
+            <main>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/login" element={<Login />} />
+                </Routes>
+            </main>
         </>
     )
 }
